7cf3c046fa619f6c542860c8aa9723cb4a81e97a
bradberger01 <bradberger@hotmail.com>
2017-09-14 20:43:57 -0400

    Hospital Project

---
+++ hospitalProject/Doctor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hospitalProject
+{
+    class Doctor:HospitalEmployee  
+    {
+        ////FIELDS
+        //protected string specialtyArea;
+
+        ////PROPERTIES
+        //public string SpecialtyArea
+        //{
+        //    get { return this.specialtyArea; }
+        //    set { this.specialtyArea = value; }
+        //}
+
+        //CONTRACTORS
+        public Doctor()
+        {
+
+        }
+        public Doctor(string employeeName, float employeeNumber, string specialtyArea)
+        {
+            this.employeeName = employeeName;
+            this.employeeNumber = employeeNumber;
+            this.specialtyArea = specialtyArea;
+        }
+
+    }
+
+}
---
+++ hospitalProject/HospitalEmployee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hospitalProject
+{
+    class HospitalEmployee
+    {
+        //FIELDS
+        protected string employeeName;
+        protected float employeeNumber;
+        //FIELDS
+        protected string specialtyArea;
+
+        //PROPERTIES
+        public string SpecialtyArea
+        {
+            get { return this.specialtyArea; }
+            set { this.specialtyArea = value; }
+        }
+
+        //PROPERTIES
+        public string Name
+        {
+            get { return this.employeeName; }
+            set { this.employeeName = value; }
+        }
+        public float idNumber
+        {
+            get { return this.employeeNumber; }
+            set { this.employeeNumber = value; }
+        }
+
+        //CONTRACTORS
+        public HospitalEmployee()
+        {
+
+        }
+
+        public HospitalEmployee(string employeeName, float employeeNumber)
+        {
+            this.employeeName = employeeName;
+            this.employeeNumber = employeeNumber;
+        }
+        //METHOD
+        public virtual void GetName()
+        {
+            Console.WriteLine("Enter your name.");
+            employeeName = Console.ReadLine();
+         
+        }
+    }
+}
